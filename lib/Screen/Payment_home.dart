0f2fd051c62f3d994d6ba0ee6b30e990ac0c379b
syed iqbal <iqbalsyed406@gmail.com>
2020-06-27 23:22:09 +0500

    stripe payment with flutter

---
+++ lib/Screen/Payment_home.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+import 'package:stripepayment/services/payment-service.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  onItemPress(BuildContext context, int index) async {
+    switch (index) {
+      case 0:
+        payViaNewCard(context);
+        break;
+      case 1:
+        Navigator.pushNamed(context, '/existing-cards');
+        break;
+    }
+  }
+
+  payViaNewCard(BuildContext context) async {
+    ProgressDialog dialog = new ProgressDialog(context);
+    dialog.style(message: 'Please wait...');
+    await dialog.show();
+    var response =
+        await StripeService.payWithNewCard(amount: '15000', currency: 'USD');
+    await dialog.hide();
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text(response.message),
+      duration:
+          new Duration(milliseconds: response.success == true ? 1200 : 3000),
+    ));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    StripeService.init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Payment Selection Screen'),
+      ),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(20, 220, 20, 0),
+        child: ListView.separated(
+            itemBuilder: (context, index) {
+              Icon icon;
+              Text text;
+
+              switch (index) {
+                case 0:
+                  icon = Icon(Icons.add_circle, color: theme.primaryColor);
+                  text = Text('Payment with New Card');
+                  break;
+                case 1:
+                  icon = Icon(Icons.credit_card, color: theme.primaryColor);
+                  text = Text('Payment with Existing card');
+                  break;
+              }
+
+              return InkWell(
+                onTap: () {
+                  onItemPress(context, index);
+                },
+                child: ListTile(
+                  title: text,
+                  leading: icon,
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => Divider(
+                  color: theme.primaryColor,
+                ),
+            itemCount: 2),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:stripepayment/Screen/existing-cards.dart';
+import 'package:stripepayment/Screen/Payment_home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => HomePage(),
+        '/existing-cards': (context) => ExistingCardsPage()
+      },
+    );
+  }
+}
